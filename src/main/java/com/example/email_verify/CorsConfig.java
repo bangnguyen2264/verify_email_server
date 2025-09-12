67f54dec11b251444bdf14438313e53d01217760
bangnguyen2264 <2251120335@ut.edu.vn>
2025-09-12 23:33:43 +0700

    fix: fix cors

--- src/main/java/com/example/email_verify/CorsConfig.java
+++ src/main/java/com/example/email_verify/CorsConfig.java
@@ -1,17 +1,31 @@
 package com.example.email_verify;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*") // Cho phép tất cả nguồn gốc
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(false); // Vô hiệu hóa credentials khi dùng allowedOrigins="*"
+@RequiredArgsConstructor
+public class CorsConfig {
+    @Value("${WEB_URL}")
+    private String WEB_URL;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(WEB_URL)); // chỉ cho phép domain FE
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(false); // để true thì không được dùng "*"
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
