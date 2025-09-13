9b20a1735f632ac56d570481c3fa11799097e68a
bangnguyen2264 <2251120335@ut.edu.vn>
2025-09-13 14:08:00 +0700

    fix: fix cors

--- src/main/java/com/example/email_verify/CorsConfig.java
+++ src/main/java/com/example/email_verify/CorsConfig.java
@@ -24,6 +24,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
                         .allowedOrigins(WEB_URL)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
